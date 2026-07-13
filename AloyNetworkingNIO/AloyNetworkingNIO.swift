7c6f31a8cadb763fa54a92adeb54dd04b4a49dc6
caggiulio <nunziogiuliocaggegi@gmail.com>
2026-07-13 14:12:13 +0200

    chore: remove iOS platform lock, add AloyNetworkingNIO target

---
+++ AloyNetworkingNIO/AloyNetworkingNIO.swift
@@ -0,0 +1,1 @@
+// AloyNetworkingNIO placeholder
--- Package.swift
+++ Package.swift
@@ -1,29 +1,29 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
   name: "AloyNetworking",
-  platforms: [.iOS(.v14)],
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
-    .library(
-      name: "AloyNetworking",
-      targets: ["AloyNetworking"]
-    ),
+    .library(name: "AloyNetworking", targets: ["AloyNetworking"]),
+    .library(name: "AloyNetworkingNIO", targets: ["AloyNetworkingNIO"]),
   ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
+    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.21.0"),
+    .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "AloyNetworking",
       dependencies: [],
       path: "AloyNetworking/"
+    ),
+    .target(
+      name: "AloyNetworkingNIO",
+      dependencies: [
+        .product(name: "AsyncHTTPClient", package: "async-http-client"),
+        .product(name: "NIOFoundationCompat", package: "swift-nio"),
+      ],
+      path: "AloyNetworkingNIO/"
     ),
     .testTarget(
       name: "AloyNetworkingTests",
